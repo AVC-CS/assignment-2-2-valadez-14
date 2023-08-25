7687ff2b72b178f087a2479f35bc58b3df24a2eb
valadez-14 <avaladez1@students.avc.edu>
2023-08-25 12:02:37 -0700

    lab2.2

--- main.py
+++ main.py
@@ -1,16 +1,22 @@
 def main():
-    """
+    
     ##################################################
     # Comlete your code here
-    Use the same variables: celsius fahrenheit 
+    #Use the same variables: celsius fahrenheit 
     ##################################################
-    """
+    celsius = 0
+    fahrenheit = 0
 
-    """
+    print("This program will convert Celsius to Fahrenheit:\n \n")
+    celsius = input('Enter a temperature in Celsius?\n')
+
+    celsius = int(celsius)
+    fahrenheit = (9 / 5 * celsius) + 32
+    print(f'You entered {celsius: .2f} Celsius and that is: {fahrenheit:.2f} in Fahrenheit')
     ########################################
     # Do not delete the return statement
     ########################################
-    """
+
     return celsius, fahrenheit
 
 
